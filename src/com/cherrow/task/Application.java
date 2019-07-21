2c04b284fc2d90e8dc2eb16c114ec34dcca47f65
cherrow <cherrow@foxmail.com>
2019-07-21 17:06:29 +0800

    initialize project, add model and utils

---
+++ src/com/cherrow/task/Application.java
@@ -0,0 +1,9 @@
+package com.cherrow.task;
+
+public class Application {
+
+    public static void main(String[] args){
+
+    }
+
+}
---
+++ src/com/cherrow/task/model/Employee.java
@@ -0,0 +1,60 @@
+package com.cherrow.task.model;
+
+import com.cherrow.task.utils.DateHandler;
+
+import java.util.Date;
+
+public class Employee {
+
+    public Employee(String lastName, String firstName, Date birthDate, String emailAddress){
+        setLastName(lastName);
+        setFirstName(firstName);
+        setBirthDate(birthDate);
+        setEmailAddress(emailAddress);
+    }
+
+    private String lastName;
+
+    private String firstName;
+
+    private Date birthDate;
+
+    private String emailAddress;
+
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    private void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    private void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    private Date getBirthDate() {
+        return birthDate;
+    }
+
+    private void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public boolean todayIsBirthday(){
+        return DateHandler.isSameMonthDateWithNow(getBirthDate());
+    }
+}
