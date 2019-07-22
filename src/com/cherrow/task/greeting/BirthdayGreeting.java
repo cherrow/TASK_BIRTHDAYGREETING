7ea7d42ae3f62d094da0d650942aa65f2d6d084a
cherrow <cherrow@foxmail.com>
2019-07-22 22:22:17 +0800

    add method to get today's birthday employees

--- src/com/cherrow/task/greeting/BirthdayGreeting.java
+++ src/com/cherrow/task/greeting/BirthdayGreeting.java
@@ -4,11 +4,19 @@ import com.cherrow.task.model.Employee;
 import com.cherrow.task.utils.EmailHandler;
 import com.cherrow.task.utils.FileHandler;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class BirthdayGreeting {
 
-    public static long sendGreetingReturnSuccessCount(){
-       return FileHandler.convertFileToEmployee().stream()
+    public static List<Employee> getIsBirthdayEmployee(){
+        return FileHandler.convertFileToEmployee().stream()
                 .filter(Employee::todayIsBirthday)
+                .collect(Collectors.toList());
+    }
+
+    public static long sendGreetingReturnSuccessCount(){
+       return getIsBirthdayEmployee().stream()
                 .filter(EmailHandler::sendEmail)
                 .count();
     }
