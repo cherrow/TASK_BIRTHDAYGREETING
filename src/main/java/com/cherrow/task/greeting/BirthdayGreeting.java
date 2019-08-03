36154fbd30e4707d98d46d53ac747d3f44f21154
cherrow <cherrow@foxmail.com>
2019-08-03 20:31:26 +0800

    merge methods

--- src/main/java/com/cherrow/task/greeting/BirthdayGreeting.java
+++ src/main/java/com/cherrow/task/greeting/BirthdayGreeting.java
@@ -1,23 +1,20 @@
 package com.cherrow.task.greeting;
 
 import com.cherrow.task.model.Employee;
-import com.cherrow.task.utils.EmailHandler;
-import com.cherrow.task.utils.FileHandler;
-
-import java.util.List;
-import java.util.stream.Collectors;
+import com.cherrow.task.model.Message;
+import com.cherrow.task.utils.EmailService;
+import com.cherrow.task.utils.FileUtil;
 
 public class BirthdayGreeting {
 
-    public static List<Employee> getIsBirthdayEmployee(){
-        return FileHandler.convertFileToEmployee().stream()
+    public static long sendGreetingReturnSuccessCount() {
+        return FileUtil.convertFileToEmployee().stream()
                 .filter(Employee::todayIsBirthday)
-                .collect(Collectors.toList());
-    }
-
-    public static long sendGreetingReturnSuccessCount(){
-       return getIsBirthdayEmployee().stream()
-                .filter(EmailHandler::sendEmail)
+                .filter(employee -> EmailService.send(employee.getEmailAddress(),
+                        Message.builder()
+                                .subject("Happy birthday!")
+                                .content("Happy birthday, dear " + employee.getFirstName())
+                                .build()))
                 .count();
     }
 }
