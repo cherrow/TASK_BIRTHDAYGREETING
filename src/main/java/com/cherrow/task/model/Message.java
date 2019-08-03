14da1924918c51d6631c0074d4c0a0f13dbff51a
cherrow <cherrow@foxmail.com>
2019-08-03 20:28:07 +0800

    extract message class

---
+++ src/main/java/com/cherrow/task/model/Message.java
@@ -0,0 +1,13 @@
+package com.cherrow.task.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Message {
+
+    private String subject;
+
+    private  String content;
+}
